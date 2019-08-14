b8c36279e3d561c570e3f8708d739eddd7e34ef6
cheratt karim <karim.cheratt@yahoo.ca>
2019-08-14 15:47:01 -0400

    fix test

--- test/eventset.test.js
+++ test/eventset.test.js
@@ -5,13 +5,13 @@ const Topic = require('../src/topic.js');
 
 describe("Test EventSet Object", function () {
 
-  it("Test EventSet.Topic() : returns Topic Instance", function () {
+  it("Test EventSet.Topic(name) : returns Topic Instance", function () {
 
     var topic = EventSet.Topic("topic-name");
     assert.strictEqual(topic instanceof Topic, true);
   });
 
-  it("Test EventSet.Topic() : returns One Topic Instance", function () {
+  it("Test EventSet.Topic(name) : returns the same Topic Instance identified by name", function () {
 
     var topicInstanceOne = EventSet.Topic('topic-name');
     var topicInstanceTwo = EventSet.Topic('topic-name');
